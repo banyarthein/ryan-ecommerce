e181ac780e5000fef1bd2176c98f4811b3b516f5
banyarthein <banyarthein@gmail.com>
2022-02-28 22:28:48 +0800

    Clean up Commit

--- server/firebase/index.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../config/fbServiceAccountKey.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://ecommerce-7cec5.firebaseio.com",
-});
-module.exports = admin
